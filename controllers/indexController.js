865dcb29c5e8d923257c6aa8a711b262dc701382
francoleone23 <fleone@udesa.edu.ar>
2021-09-26 23:23:31 -0300

    error

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,7 +4,7 @@ const comentarios = require('../module/comentarios');
 
 const controller = {
   mostrarIndex: function (req, res) {
-    res.render("views/index", {posts: posts.list, comments: comments.list, users: users.list});
+    res.render("views/index", {posts: posts.posts, comentarios: comentarios.comentarios, users: users.users});
   },
   mostrarResultadoBusqueda: function (req, res) {
     res.render("views/resultadoBusqueda");
