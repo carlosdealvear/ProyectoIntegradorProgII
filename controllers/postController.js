36cf86e331e212049d39fbdb6c0edac08d0566bb
francoleone23 <fleone@udesa.edu.ar>
2021-09-27 00:14:05 -0300

    Update postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,15 +1,15 @@
 const posts = require('../module/posts');
 const users = require('../module/users');
-const comments = require('../module/comentarios');
+const comentarios = require('../module/comentarios');
 const controller = {
   mostrarAgregarPost: function (req, res) {
     res.render("agregarPost");
   },
   mostrarDetallePost: function (req, res) {
-    for (let i = 0; i < posts.list.length; i++) {
-      const element = posts.list[i];
+    for (let i = 0; i < posts.posts.length; i++) {
+      const element = posts.posts[i];
       if (element.id == req.params.id) {
-        res.render("detallesPost", { posts: element });
+        res.render("detallePost", { posts: element });
       }
     }
   },
