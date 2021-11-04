851d6efdb2dccf5833065eebe5db0ae4f3dde347
francoleone23 <fleone@udesa.edu.ar>
2021-11-04 11:23:46 -0300

    config

--- database/config/config.js
+++ database/config/config.js
@@ -1,23 +1,8 @@
-module.exports ={
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+   "username": "root",
+   "database": "PROYECTPII",
+   "host": "127.0.0.1",
+   "dialect": "mysql"
+ }
 }
