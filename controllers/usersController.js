fcdf23766f91850df66e4791fefdc2655eb5a916
francoleone23 <fleone@udesa.edu.ar>
2021-11-06 17:04:16 -0300

    registracion controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,7 @@
 const posts = require('../module/posts');
 const users = require('../module/users');
-const comentarios = require('../module/comentarios');
-
+let db = require('../database/models');
+let bcrypt = require('bcryptjs');
 
 const usersController = {
   mostrarLogin: function (req, res) {
@@ -31,4 +31,61 @@ const usersController = {
   },
 };
 
+
+
+
+// registracion
+const registracionController = {
+
+	registracion: function (req, res) {
+		res.render('registracion')
+	},
+	registrar: function (req, res) {
+		let errores = {}
+		let existeMail;
+
+		db.users.findAll({
+				where: {
+					email: req.body.email
+				}
+			})
+			.then(function (email) {
+
+				existeMail = email;
+
+				if (req.body.email == "") {
+					errores.message = "No puede estar vacio este campo";
+					res.locals.error = errores;
+					res.render("registracion")
+				} else if (req.body.password == "") {
+					errores.message = "Falta la contraseña!!";
+					res.locals.error = errores;
+					res.render("registracion")
+				} else if (existeMail != "") { // != significa not equal to 
+					errores.message = "Este mail ya esta en uso.";
+					res.locals.error = errores;
+					res.render("registracion")
+				} else {
+					let encriptPass = bcrypt.hashSync(req.body.password, 10)
+					db.users.create({
+							nombre: req.body.username,
+							email: req.body.email,
+							password: encriptPass,
+							fecha: req.body.fecha,
+							createdAt: Date.now()
+						})
+						.then(users => {
+							res.redirect('/')
+						})
+						.catch(err => {
+							console.log(err);
+							res.send(err)
+						})
+				}
+			})
+
+	},
+}
 module.exports = usersController;
+
+module.exports = registracionController;
