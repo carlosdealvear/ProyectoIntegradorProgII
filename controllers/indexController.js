eb771fb0f83cb3129920888f10060e5ebad6a085
francoleone23 <fleone@udesa.edu.ar>
2021-11-22 16:47:30 -0300

    push

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,32 +1,50 @@
-const posts = require('../module/posts');               
+const posts = require('../module/posts');
 const users = require('../module/users');
 const comentarios = require('../module/comentarios');
-const {json}= require("express")
+const {
+  json
+} = require("express")
 let db = require('../database/models');
 op = db.Sequelize.Op
 
-const indexController = {                               
+const indexController = {
   mostrarIndex: function (req, res) {
-    res.render('index', {posts: posts.posts, comentarios: comentarios.comentarios, users: users.users});
+    res.render('index', {
+      posts: posts.posts,
+      comentarios: comentarios.comentarios,
+      users: users.users
+    });
+
   },
   mostrarResultadoBusqueda: function (req, res) {
     let buscado = req.query.search
-    let resultados =[]
+    let resultados = []
     db.posts.findAll({
-      where: {[op.or]: [{descrip: {[op.like]: `%${buscado}%`}}]}, 
-      include: [{association:"creador"}]
-    })
-    .then((result)=> {
-      for (let index = 0; index < result.length; index++) {
-        resultados.push(result[index].dataValues)
-        
-      }
-      return res.render('resultadoBusqueda', {buscado:buscado, resultados:result})
-    })
-    .catch((error)=>{
-      return res.send(error)
-    })
+        where: {
+          [op.or]: [{
+            descrip: {
+              [op.like]: `%${buscado}%`
+            }
+          }]
+        },
+        include: [{
+          association: "creador"
+        }]
+      })
+      .then((result) => {
+        for (let index = 0; index < result.length; index++) {
+          resultados.push(result[index].dataValues)
+
+        }
+        return res.render('resultadoBusqueda', {
+          buscado: buscado,
+          resultados: result
+        })
+      })
+      .catch((error) => {
+        return res.send(error)
+      })
   },
 };
 
-module.exports = indexController;      
+module.exports = indexController;
