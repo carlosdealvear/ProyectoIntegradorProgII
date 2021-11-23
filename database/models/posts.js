d7c3b50aa8142e042825bf2f744082c0f88a2e7e
francoleone23 <fleone@udesa.edu.ar>
2021-11-22 22:11:36 -0300

    faltaba una asociacion

--- database/models/posts.js
+++ database/models/posts.js
@@ -1,39 +1,44 @@
-module.exports = function(sequelize, dataTypes){
+module.exports = function (sequelize, dataTypes) {
 	let alias = 'posts';
 	let columnas = {
-	    id:{
-		autoIncrement: true,
-		primaryKey: true,
-		type: dataTypes.INTEGER,
-	    },
-	    id_usuario:{
-		type: dataTypes.INTEGER,
-	    },
-	    img:{
-		type: dataTypes.STRING,
-	    },
-	    descrip:{
-		type: dataTypes.STRING,
-	    },
-	    fecha:{
-		type: dataTypes.DATE,
-	    }
-	}
-    
-	let conf= {
-	    tableName: 'posteo', 
-	    timestamps: false, 
-	    underscored: true,        
-	}
-	
-    
-       const posts = sequelize.define(alias, columnas, conf);
-		posts.associate= function(models){
-
-        posts.belongsTo(models.usuario, {
-            as: "creador",
-            foreignKey: "id_usuario"
-        })
+		id: {
+			autoIncrement: true,
+			primaryKey: true,
+			type: dataTypes.INTEGER,
+		},
+		id_usuario: {
+			type: dataTypes.INTEGER,
+		},
+		img: {
+			type: dataTypes.STRING,
+		},
+		descrip: {
+			type: dataTypes.STRING,
+		},
+		fecha: {
+			type: dataTypes.DATE,
 		}
-       return posts;
-    }
+	}
+
+	let conf = {
+		tableName: 'posteo',
+		timestamps: false,
+		underscored: true,
+	}
+
+
+	const posts = sequelize.define(alias, columnas, conf);
+	posts.associate = function (models) {
+		posts.hasMany(models.comentarios, {
+			as: "Comentarios",
+			foreignKey: "id_posteo",
+		});
+		posts.belongsTo(models.usuario, {
+			as: "creador",
+			foreignKey: "id_usuario"
+		})
+
+	}
+
+	return posts;
+}
